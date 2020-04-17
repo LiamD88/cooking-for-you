0fc6bf538f1195fadfd0e76c7e30545f0d329ef5
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-04-17 22:10:27 +0100

    I had to correct spelling off stringfield and equal to in the text to camel case as was causign an error

--- forms.py
+++ forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, Stringfield, validators, PasswordField, BooleanField
+from wtforms import Form, StringField, validators, PasswordField, BooleanField
 
-class RegisterForm(Form):
-    name = Stringfield('Name', [validators.length(min=2, max= 40)])
-    username = Stringfield('Username', [validators.length(min=3, max =20)])
-    email = Stringfield('Email', [validators.length(min=5, max =40)])
+class RegisterForm(FlaskForm):
+    name = StringField('Name', [validators.length(min=2, max= 40)])
+    username = StringField('Username', [validators.length(min=3, max =20)])
+    email = StringField('Email', [validators.length(min=5, max =40)])
     password = PasswordField('Password', [validators.DataRequired(), validators.length(min=5, max=20)])
-    confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.length(min=5, max=20)], validators.Equalto('password', message = "Passwords must match."))
+    confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.length(min=5, max=20)], validators.EqualTo('password', message = "Passwords must match."))
 
 
-class LoginForm(Form):
-    username = Stringfield('Username', [validators.DataRequired()])
-    email = Stringfield('Email', [validators.DataRequired()])
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired()])
+    email = StringField('Email', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
     confirm_password = PasswordField('Confirm Password', [validators.DataRequired()])
 
