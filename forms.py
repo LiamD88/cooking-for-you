8be4eab1ae6468f06af617cf80dbfb5c277630b5
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-04-18 14:56:50 +0100

    I have updated my form.py to include a submit button and upon more research online a better way to use the validators and have inported wtf forms validator and updated my classes accordingly

--- forms.py
+++ forms.py
@@ -1,18 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, StringField, validators, PasswordField, BooleanField
+from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 
 class RegisterForm(FlaskForm):
-    name = StringField('Name', [validators.length(min=2, max=40)])
-    username = StringField('Username', [validators.length(min=3, max=20)])
-    email = StringField('Email', [validators.length(min=5, max =40)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.length(min=5, max=20)])
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Length(min=5, max=40)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_2 = PasswordField(
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
    
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', [validators.DataRequired()])
-    email = StringField('Email', [validators.DataRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
     
 
 
