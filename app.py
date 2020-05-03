4a111a66ebed2f42ef141447bf737903cd231d33
Liam Dudley <liamdudley@Liams-MacBook-Pro.local>
2020-05-03 18:15:30 +0100

    I changed debug mode to false

--- app.py
+++ app.py
@@ -317,4 +317,4 @@ def delete_recipe_vegetarian(vegetarian_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', "0.0.0.0"),
     port=int(os.environ.get('PORT', "5000")),
-    debug=True)
+    debug=False)
